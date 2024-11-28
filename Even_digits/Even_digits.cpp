4e738ee0ead213fdece1632f9c9861bab0f4322e
Andrei_Gridin <andreigridin918@gmail.com>
2024-11-28 20:12:22 +0500

    Добавьте файлы проекта.

---
+++ Even_digits/Even_digits.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+void evendigits(long long& nums, int& ans)
+{
+	int c = 10;
+	if ((nums % c) % 2 == 0)
+	{
+		ans++;
+	}
+	nums /= c;
+	while (nums > 0)
+	{
+		evendigits(nums, ans);
+	}
+}
+
+int main()
+{
+	long long n = 9223372036854775806;
+	int a = 0;
+	evendigits(n, a);
+	std::cout << ' ' << a << std::endl;
+}
